c086754d70f8fd148541a24cafdc9b007909ccb7
arx-8 <swans.randomness@gmail.com>
2022-05-25 22:51:01 +0900

    feat: try victory-native

--- src/Sample.tsx
+++ src/Sample.tsx
@@ -1,9 +1,12 @@
 import { StyleSheet, Text, View } from "react-native"
+import { Victory } from "src/packages/victory"
 
 export const Sample = (): JSX.Element => {
   return (
     <View>
       <Text style={styles.text}>Hello Sample component</Text>
+
+      <Victory.VictoryPolarAxis />
     </View>
   )
 }
